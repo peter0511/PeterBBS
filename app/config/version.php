5110f17359e5d3f113eb821c40c85a43019fb25c
zengpan <zengpan@ymt360.com>
2015-05-08 16:38:02 +0800

    fix

---
+++ app/config/version.php
@@ -0,0 +1,9 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config = array (
+  'sys_name' => 'StartBBS',
+  'sys_url' => 'http://www.startbbs.com',
+  'sys_version' => 'V1.3.0',
+  'sys_update' => '20150201',
+  'sys_author' => 'QQ858292510',
+);
